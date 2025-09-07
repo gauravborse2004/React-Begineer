aa6a316ab0770d8d92cb12fcca1665ecd38ccbfd
gauravborse2004 <gauravsborse08@gmail.com>
2025-09-07 16:30:35 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@
 // import './App.css';
 import Navbar from './navbar';
 import Home from "./Home";
+import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Create from './Create';
+import BlogDetails from './BlogDetails';
 
 function App() {
   const title = "Hello, Dear Gaurav";
@@ -9,24 +12,30 @@ function App() {
   const link = "https://www.google.com";
 
   return (
-    <div className="App">
-      <Navbar></Navbar>
-      
+    <Router>
+      <div className="App">
+        <Navbar></Navbar>
+        
 
-      <div className="content">
+        <div className="content">
 
-        <Home></Home>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create" element={<Create />} />
+            <Route path="/blogs/:id" element={<BlogDetails />} />
+          </Routes>
 
-        {/* <h1>{ title }</h1>
-        <p>likes: {likes} people</p>
-        <p>{ [1,2,3,4,5,6,7,8] }</p>
-        <p>{ Math.random() * 20}</p>
+          {/* <h1>{ title }</h1>
+          <p>likes: {likes} people</p>
+          <p>{ [1,2,3,4,5,6,7,8] }</p>
+          <p>{ Math.random() * 20}</p>
 
-        <a href="https://www.google.com">Google</a><br />
+          <a href="https://www.google.com">Google</a><br />
 
-        <a href={link}>Google Site</a> */}
+          <a href={link}>Google Site</a> */}
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
